e8e85b19b69012defea0a2e73fc7b5386175e031
readybuilderone <neohan2016@outlook.com>
2021-08-21 13:38:14 +0800

    initial projen lib

---
+++ source/.projenrc.js
@@ -0,0 +1,19 @@
+const { AwsCdkConstructLibrary } = require('projen');
+const project = new AwsCdkConstructLibrary({
+  author: 'readybuilderone',
+  authorAddress: 'neohan2016@outlook.com',
+  cdkVersion: '1.95.2',
+  defaultReleaseBranch: 'main',
+  name: 'source',
+  repositoryUrl: 'https://github.com/readybuilderone/serverless-airflow.git',
+
+  // cdkDependencies: undefined,        /* Which AWS CDK modules (those that start with "@aws-cdk/") does this library require when consumed? */
+  // cdkTestDependencies: undefined,    /* AWS CDK modules required for testing. */
+  // deps: [],                          /* Runtime dependencies of this module. */
+  // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],                       /* Build dependencies for this module. */
+  // packageName: undefined,            /* The "name" in package.json. */
+  // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
+  // release: undefined,                /* Add release management to this project. */
+});
+project.synth();
